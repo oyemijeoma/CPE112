2c8d0905691f9bd5438c51625b913a635b5f1174
oyemijeoma <oyemijeoma2@gmail.com>
2026-07-30 10:58:40 +0100

    Add files via upload

---
+++ #Oyem Ijeoma.py
@@ -0,0 +1,16 @@
+#Oyem Ijeoma
+#ENG2507743
+import tkinter as tkinter
+from tkinter import messsage box 
+
+
+def login()
+    username = entry_username.get()
+    password = entry_password.get()
+    
+    #example check 
+    if username == "admin" and password == "1234":
+        messagebox.showinfo("login succesful,f"Welcome, {username}!)
+    else:
+        messagebox.showerror("login Failed")
+    
